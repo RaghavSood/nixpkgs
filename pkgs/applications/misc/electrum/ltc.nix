c49b8d9cabdf3269f6319121b75ad6296d2a3504
Raghav Sood <r@raghavsood.com>
2020-10-29 17:55:09 +0800

    upstream updatE

--- pkgs/applications/misc/electrum/ltc.nix
+++ pkgs/applications/misc/electrum/ltc.nix
@@ -1,22 +1,56 @@
 { stdenv
-, fetchurl
+# , fetchurl
+, secp256k1
+, enableQt ? true
+, fetchFromGitHub
 , python3Packages
 , wrapQtAppsHook
+, python3
 }:
 
+let
+  py = python3.override {
+    packageOverrides = self: super: {
+      dnspython = super.dnspython_1;
+    };
+  };
+
+  libsecp256k1_name =
+    if stdenv.isLinux then "libsecp256k1.so.0"
+    else if stdenv.isDarwin then "libsecp256k1.0.dylib"
+    else "libsecp256k1${stdenv.hostPlatform.extensions.sharedLibrary}";
+
+in
 python3Packages.buildPythonApplication rec {
   pname = "electrum-ltc";
-  version = "3.3.8.1";
+  version = "e25217f77b928637b00f5cf7e061f4832c6f7641";
 
-  src = fetchurl {
-    url = "https://electrum-ltc.org/download/Electrum-LTC-${version}.tar.gz";
-    sha256 = "0kxcx1xf6h9z8x0k483d6ykpnmfr30n6z3r6lgqxvbl42pq75li7";
+  #src = fetchurl {
+  #  url = "https://electrum-ltc.org/download/Electrum-LTC-${version}.tar.gz";
+  #  sha256 = "1kxcx1xf6h9z8x0k483d6ykpnmfr30n6z3r6lgqxvbl42pq75li7";
+  #};
+
+  src = fetchFromGitHub {
+    owner = "pooler";
+    repo = pname;
+    rev = "${version}";
+    sha256 = "1m8y5fjpc6l1ypvmxkvqs82y80n7zk5wsy0ax7d6aidqallimgdg";
   };
 
   nativeBuildInputs = with python3Packages; [ pyqt5 wrapQtAppsHook ];
 
-  propagatedBuildInputs = with python3Packages; [
+  propagatedBuildInputs = with py.pkgs; [
+    aiohttp
+    aiohttp-socks
+    aiorpcx
+    attrs
     pyaes
+    bitstring
+    dnspython
+    ecdsa
+    jsonrpclib-pelix
+    matplotlib
+    pbkdf2
     ecdsa
     pbkdf2
     requests
@@ -29,13 +63,16 @@ python3Packages.buildPythonApplication rec {
     pysocks
     trezor
     btchip
-  ];
+  ] ++ stdenv.lib.optionals enableQt [ pyqt5 qdarkstyle ];
 
   preBuild = ''
     sed -i 's,usr_share = .*,usr_share = "'$out'/share",g' setup.py
-    pyrcc5 icons.qrc -o gui/qt/icons_rc.py
+    ls -al
+    substituteInPlace ./electrum_ltc/ecc_fast.py \
+      --replace ${libsecp256k1_name} ${secp256k1}/lib/libsecp256k1${stdenv.hostPlatform.extensions.sharedLibrary}
+    # pyrcc5 icons.qrc -o gui/qt/icons_rc.py
     # Recording the creation timestamps introduces indeterminism to the build
-    sed -i '/Created: .*/d' gui/qt/icons_rc.py
+    # sed -i '/Created: .*/d' gui/qt/icons_rc.py
   '';
 
   preFixup = ''
